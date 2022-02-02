c99c210f7c65f4cf9ca30a32b3dd961ac60dd315
DESKTOP-3S9NH3M\m3zo0 <alexfeiwork@gmail.com>
2022-02-02 12:20:40 +0800

    add java class

---
+++ src/testJavaCompiler.java
@@ -0,0 +1,9 @@
+
+public class testJavaCompiler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test java compiler and git tool");
+	}
+
+}
