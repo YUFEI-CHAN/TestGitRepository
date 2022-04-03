313b927facf0ad61a73e614c459a355ce19a150a
DESKTOP-3S9NH3M\m3zo0 <alexfeiwork@gmail.com>
2022-04-03 18:13:28 +0800

    newBranch merge

--- src/testJavaCompiler.java
+++ src/testJavaCompiler.java
@@ -4,6 +4,11 @@ public class testJavaCompiler {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test java compiler and git tool");
+		
+		System.out.println("測試");
+		for(int i = 0; i<20 ;i++) {
+			System.out.println(1+i);
+		}
 	}
 
 }
