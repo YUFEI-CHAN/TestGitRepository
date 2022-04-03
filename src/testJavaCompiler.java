b05251f042df72dfe5911b26f2d6f5b5625c84ce
DESKTOP-3S9NH3M\m3zo0 <alexfeiwork@gmail.com>
2022-04-03 18:11:26 +0800

    for loo[ test

--- src/testJavaCompiler.java
+++ src/testJavaCompiler.java
@@ -4,6 +4,11 @@ public class testJavaCompiler {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test java compiler and git tool");
+		
+		Integer e = 2;
+		for(int i = 0 ; i< 21 ; i++) {
+			System.out.println(e+i);
+		}
 	}
 
 }
